378313908f2adffdc875fd034675ee47c4790311
Gregory Foss <gregfoss@Gregorys-MacBook-Pro.local>
2018-08-27 12:01:10 -0400

    Adding the basic runtime structure to the main class and main method.

--- game/src/runtime/Runtime.java
+++ game/src/runtime/Runtime.java
@@ -2,7 +2,60 @@ package runtime;
 
 public class Runtime {
 	
+	boolean running = false;
+	
+	public Runtime() {
+		
+	}
+	
+	/**
+	 * The getter for the flag that indicates whether or not a new game loop should be run
+	 * before calling the quit() and clean_up() functions.
+	 * @return a boolean.
+	 */
+	public boolean keep_running() {
+		return this.running;
+	}
+	
+	/**
+	 * Should be called before the loop method is called.
+	 */
+	public void start() {
+		this.running = true;
+	}
+	
+	/**
+	 * Executes one iterations of the game loop.
+	 */
+	public void loop() {
+		
+	}
+	
+	/**
+	 * Quits the game.
+	 */
+	public void quit() {
+		
+	}
+	
+	/**
+	 * Cleans up any open resources.
+	 */
+	public void clean_up() {
+		
+	}
+	
 	public static void main(String[] args) {
+		
+		Runtime game = new Runtime();
+		game.start();
+		
+		while (game.keep_running()) {
+			game.loop();
+		}
+		
+		game.quit();
+		game.clean_up();
 		
 	}
 
